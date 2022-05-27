ed8f422802a5f9934e58a885ce5d14f3df7a2d32
bagubhargav <40735360+bagubhargav@users.noreply.github.com>
2022-05-27 15:17:27 +1000

    Update index.js

--- Dicee Challenge - Completed/index.js
+++ Dicee Challenge - Completed/index.js
@@ -25,5 +25,5 @@ else if (randomNumber2 > randomNumber1) {
   document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
 }
 else {
-  document.querySelector("h1").innerHTML = "Draw!";
+  document.querySelector("h1").innerHTML = "Draw!!";
 }
